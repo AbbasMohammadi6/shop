2e5d40139e89cee3860087fce40b68b59d6767e7
Abbas Mohammadi <abbasmohammadi6@gmail.com>
2021-04-24 21:51:47 +0430

    style ChatScreen

--- frontend/src/screens/ChatScreen.js
+++ frontend/src/screens/ChatScreen.js
@@ -19,45 +19,109 @@ const Aside = styled.aside`
 
 const UserLi = styled.li`
   background: ${({ currentUser }) => currentUser && "springgreen !important"};
-
+  border-radius: 0.8rem;
   padding: 0.5rem;
+  text-align: center;
+  margin-bottom: 0.5rem;
+  color: #333;
+
+  @media (max-width: 600px) {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    width: 3rem;
+  }
 
   &:hover {
     cursor: pointer;
   }
 
   &:nth-child(odd) {
-    background: #dadada;
+    background: beige;
   }
 
   &:nth-child(even) {
-    background: #fafafa;
+    background: antiquewhite;
   }
 `;
 
 const Chats = styled.div`
   flex: 3;
+
+  & ul {
+    padding-right: 1rem;
+    list-style: none;
+  }
 `;
 
 const MsgLi = styled.li`
-  background: ${({ fromSelf }) => (fromSelf ? "springgreen" : "tomato")};
-  width: 30rem;
+  background: ${({ fromSelf }) => (fromSelf ? "#56e29c" : "#f38572")};
+  margin: 1rem 0;
+  margin-right: ${({ fromSelf }) => (fromSelf ? "auto" : "0")};
+  width: 15rem;
+  padding: 1rem;
+  border-radius: 5px;
+  position: relative;
+
+  @media (max-width: 450px) {
+    width: 10rem;
+  }
+
+  & span {
+    position: absolute;
+    top: 2px;
+    left: 5px;
+    font-size: 0.7rem;
+    color: #666;
+  }
 `;
 
 const Form = styled.form`
   position: fixed;
-  /* DELETE THIS LATER: because the containers width is 80%, if we want to make the form align with contianer we have to make left 10% and right 10%, if we want the form to be under the Chats section, we have to add that 25% of 80% to the right, so 25% of 80% is 20% of 100% so right becomes 20%+10% = 30%  */
   left: 10%;
-  right: 30%;
+  right: 10%;
   bottom: 0;
+  max-width: 1000px; /* equal to Container's max-width */
+  margin: auto; /* for when screen width is more than 1000px */
+  height: 3rem;
   display: flex;
+  border: solid 1px #ccc;
+  border-radius: 2rem;
+  overflow: hidden;
 
   & textarea {
-    width: 85%;
+    width: 92%;
+    border: none;
+    outline: none;
+    resize: none;
+    font-family: inherit;
+    font-size: 0.85rem;
+    overflow: auto;
+    padding-right: 1.5rem;
+
+    @media (max-width: 800px) {
+      width: 88%;
+    }
   }
 
   & button {
-    width: 15%;
+    width: 8%;
+    border: none;
+    outline: none;
+    border-right: 1px solid #ccc;
+    color: palevioletred;
+    background: white;
+    transition: all 200ms;
+    font-size: 1.2rem;
+
+    @media (max-width: 800px) {
+      width: 12%;
+    }
+
+    &:hover {
+      background: palevioletred;
+      color: white;
+    }
   }
 `;
 
@@ -115,7 +179,7 @@ const ChatScreen = ({ history }) => {
     <Main>
       <Aside>
         <ul>
-          {users.map((user, idx) => {
+          {/* {users.map((user, idx) => {
             if (!user.isAdmin)
               return (
                 <UserLi
@@ -127,31 +191,92 @@ const ChatScreen = ({ history }) => {
                 </UserLi>
               );
             else return null;
-          })}
+          })} */}
+
+          <UserLi>2452sdfbsdfg34432</UserLi>
+          <UserLi>2452sdfbsdfg34432</UserLi>
+          <UserLi>2452sdfbsdfg34432</UserLi>
+          <UserLi>2452sdfbsdfg34432</UserLi>
+          <UserLi>2452sdfbsdfg34432</UserLi>
+          <UserLi>2452sdfbsdfg34432</UserLi>
         </ul>
       </Aside>
 
       <Chats>
         <ul>
-          {msgs.map(({ message, from, to }, idx) => {
+          {/* msgs.map(({ message, from, to }, idx) => {
             if (from === currentUser || to === currentUser)
               return (
                 <MsgLi key={idx} fromSelf={from === socket.id}>
                   {message}
+                  <span>{(from === socket.id) ? 'admin' : 'user'}</span>
                 </MsgLi>
               );
             else return null;
-          })}
+          }) */}
+
+          <MsgLi fromSelf={false}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+            <span>{false ? "admin" : "user"}</span>
+          </MsgLi>
+          <MsgLi fromSelf={true}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+            <span>{true ? "admin" : "user"}</span>
+          </MsgLi>
+          <MsgLi fromSelf={false}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+            <span>{false ? "admin" : "user"}</span>
+          </MsgLi>
+          <MsgLi fromSelf={true}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+            <span>{true ? "admin" : "user"}</span>
+          </MsgLi>
+          <MsgLi fromSelf={false}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
+          <MsgLi fromSelf={true}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
+          <MsgLi fromSelf={false}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
+          <MsgLi fromSelf={true}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
+          <MsgLi fromSelf={false}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
+          <MsgLi fromSelf={true}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
+          <MsgLi fromSelf={false}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
+          <MsgLi fromSelf={true}>
+            ن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع
+            کلاس اول
+          </MsgLi>
         </ul>
       </Chats>
-
+      {/* جاوااسکریپت با نوعی زبان برنامه‌نویسی است که با ویژگی‌های ارائه شده در مشخصات اکما اسکریپت مطابق می‌باشد. جاوااسکریپت نوعی زبان سطح بالا، کامپایل درجا، و چندالگویی است. جاوااسکریپت نحو آکولادی دارد، نوع دهی آن پویا است، نوع شیءگرایی اش بر پایه پیش‌نمونه است، و دارای توابع کلاس اول */}
       <Form>
         <textarea
           value={currentMsg}
           onChange={(e) => setCurrentMsg(e.target.value)}
         />
         <button onClick={handleSend} disabled={!currentUser || !currentMsg}>
-          ارسال
+          <i className="fas fa-paper-plane"></i>
         </button>
       </Form>
     </Main>
